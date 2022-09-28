fafc8846b6a3e90ddc39d769e3f0739b44d2eb1a
SayaliVarkhede <sayalivarkhede96@gmail.com>
2022-09-28 18:39:33 +0530

    committed method of Opera Browser

--- Facebook2CrossBrowserTesting/src/test/java/setup/Base.java
+++ Facebook2CrossBrowserTesting/src/test/java/setup/Base.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 public class Base {
 	public static WebDriver openChromeBrowser()
@@ -27,7 +28,8 @@ public class Base {
 	}
 	/*public static void openOperaBrowser()
 	{
-		System.setProperty("webdriver.chrome.driver","D:\\Selenium\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+		System.setProperty("webdriver.Opera.driver","D:\\Selenium\\Operadriver.exe");
+		WebDriver driver=new OperaDriver();
+		return driver;
 	}*/
 }
